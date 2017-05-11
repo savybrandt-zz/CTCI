b8ae06a8a0565578f31531f48339447d1955b360
savybrandt <savybrandt@gmail.com>
2017-05-11 12:25:39 -0700

    fixed typos

--- Chapter 1/checkPermutation/checkPermutation.js
+++ Chapter 1/checkPermutation/checkPermutation.js
@@ -2,11 +2,11 @@
 
 String.prototype.checkPermutation = function(string) {
 	if(this.length !== string.length) { return false; }
-	var srt1 = {};
+	var str1 = {};
 	var str2 = {};
 	for(var i = 0; i < this.length; i++) {
-		srt1[this[i]] = srt1[this[i]]+1 || 1;
-		srt2[string[i]] = srt2[string[i]]+1 || 1;
+		str1[this[i]] = str1[this[i]]+1 || 1;
+		str2[string[i]] = str2[string[i]]+1 || 1;
 	}
 	for(var key in str1) {
 		if(str1[key] !== str2[key]) { return false;}
