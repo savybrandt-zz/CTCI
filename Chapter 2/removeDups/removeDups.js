9842c90b0e46e870196be0b9afa99840d5e10511
savybrandt <savybrandt@gmail.com>
2017-05-17 12:30:09 -0700

    removeDups after debugging

--- Chapter 2/removeDups/removeDups.js
+++ Chapter 2/removeDups/removeDups.js
@@ -7,11 +7,29 @@ var removeDups = function(list) {
 		if(!storage[node.next.value]) {
 			storage[node.next.value] = 1;
 		} else {
+			//node.next.value = null; //forgot to add thisin whiteboarded solution
+			node.next = node.next.next;
+		}
+		//traverse(node.next); //forgot to add thisin whiteboarded solution
+	}
+	return traverse(list);
+}
+
+// after debugging in console
+
+var removeDups = function(list) {
+	var storage = {};
+	storage[list.value] = 1;
+	var traverse = function(node) {
+		if(node.next === null) { return list; }
+		if(!storage[node.next.value]) {
+			storage[node.next.value] = 1;
+		} else {
 			node.next.value = null; //forgot to add thisin whiteboarded solution
 			node.next = node.next.next;
 		}
 		traverse(node.next); //forgot to add thisin whiteboarded solution
 	}
-	return traverse(list);
+  traverse(list);
+ 	return list; //had to return list here instead of retutning function invocation
 }
-
